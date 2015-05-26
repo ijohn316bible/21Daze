4a717d3bfe391c7e59764cf513d94fef0ae01f1e
ijohn316bible <ijohn316bible@gmail.com>
2015-05-25 21:34:07 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,45 @@
+<?php
+/*
+UserCake Version: 2.0.2
+http://usercake.com
+*/
+echo "
+<!DOCTYPE html PUBLIC '-//W3C//DTD XHTML 1.0 Transitional//EN' 'http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd'>
+<html xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+<meta http-equiv='Content-Type' content='text/html' charset='utf-8' />
+<meta name='viewport' content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no'>
+<meta name='description' content='21Daze is a platform for men to find accountability in their walk with God. 
+The mobile-friendly web app combines many features, such as daily Bible 
+reading and a daily scheduled VoIP conference call.' />
+<meta name='theme-color' content='#000000'>
+<title>".$websiteName."</title>
+<link rel='shortcut icon' href='/models/site-templates/images/favicon.ico' type='image/x-icon'>
+<link rel='icon' href='/models/site-templates/images/favicon.ico' type='image/x-icon'>
+<link rel='apple-touch-icon' sizes='57x57' href='/models/site-templates/images/apple-icon-57x57.png'>
+<link rel='apple-touch-icon' sizes='60x60' href='/models/site-templates/images/apple-icon-60x60.png'>
+<link rel='apple-touch-icon' sizes='72x72' href='/models/site-templates/images/apple-icon-72x72.png'>
+<link rel='apple-touch-icon' sizes='76x76' href='/models/site-templates/images/apple-icon-76x76.png'>
+<link rel='apple-touch-icon' sizes='114x114' href='/models/site-templates/images/apple-icon-114x114.png'>
+<link rel='apple-touch-icon' sizes='120x120' href='/models/site-templates/images/apple-icon-120x120.png'>
+<link rel='apple-touch-icon' sizes='144x144' href='/models/site-templates/images/apple-icon-144x144.png'>
+<link rel='apple-touch-icon' sizes='152x152' href='/models/site-templates/images/apple-icon-152x152.png'>
+<link rel='apple-touch-icon' sizes='180x180' href='/models/site-templates/images/apple-icon-180x180.png'>
+<link rel='icon' type='image/png' sizes='192x192'  href='/models/site-templates/images/android-icon-192x192.png'>
+<link rel='icon' type='image/png' sizes='32x32' href='/models/site-templates/images/favicon-32x32.png'>
+<link rel='icon' type='image/png' sizes='96x96' href='/models/site-templates/images/favicon-96x96.png'>
+<link rel='icon' type='image/png' sizes='16x16' href='/models/site-templates/images/favicon-16x16.png'>
+<link rel='manifest' href='/manifest.json'>
+<meta name='msapplication-TileColor' content='#000000'>
+<meta name='msapplication-TileImage' content='/models/site-templates/images/ms-icon-144x144.png'>
+<link href='".$template."' rel='stylesheet' type='text/css' />
+<script src='models/funcs.js' type='text/javascript'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:700,300' rel='stylesheet' type='text/css'>
+</script>
+<script src='models/phone.js' type='text/javascript'> </script>
+<script src='models/jquery.countdown.js' type='text/javascript'> </script>
+<script src='models/jquery.countdown.min.js' type='text/javascript'> </script>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+</head>";
+
+?>
